fa1bef211457a76103b6c5c7f0aa17c32e8c5f4b
Wayne <yangwayne831@gmail.com>
2025-03-29 04:04:28 +0800

    update

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -36,21 +36,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
-        // basket. Note that you are not allowed to put any type of fruit that's
-        // already present!
-
-        // match fruit {
-            // Fruit::Banana => basket.insert(Fruit::Banana, 5),
-            // Fruit::Pineapple => basket.insert(Fruit::Pineapple, 10),
-            // _ => {},
-
-        if !basket.contains_key(&fruit) {
-            basket.insert(fruit, match fruit {
-                Fruit::Banana => 5,
-                Fruit::Pineapple => 10,
-                _ => 0,  // 这里不会被执行，但保持完整性
-            });
+        match fruit {
+            Fruit::Banana => {basket.insert(Fruit::Banana, 5);},
+            Fruit::Pineapple => {basket.insert(Fruit::Pineapple, 10);},
+            _ => {},
         }
     }
 }
